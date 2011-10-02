adb0329f29866779ca423f8bf50e733f86a40cc1
George Johnston <hankrearden1221@gmail.com>
2011-10-01 22:34:00 -0400

    Add Party constructor

--- src/model/Party.java
+++ src/model/Party.java
@@ -2,5 +2,17 @@ package model;
 
 public class Party {
 
-	private Person[] members = new Person[4];
+	private Person[] members;
+	
+	public Party(){
+		this.members = null;
+	}
+	
+	public Party(Person ... party){
+		this.members = party.clone();
+	}
+	
+	public Person[] getPartyMembers(){
+		return this.members;
+	}
 }
