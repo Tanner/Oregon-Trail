b731b2f41e0239d89cd54c211386fb7d84830704
Jeremy Grebner <Computer@computername>
2011-10-02 00:30:26 -0400

    Add Spinner class. Add up/down png image files. Update Label to extend Component, and fix its render method.

--- src/component/Label.java
+++ src/component/Label.java
@@ -1,29 +1,66 @@
 package component;
 
-import org.newdawn.slick.Font;
+import org.newdawn.slick.*;
+import org.newdawn.slick.gui.*;
 
-public class Label {
+public class Label extends Component {
 	
 	private String text;
 	private Font font;
 	private int x, y;
+	private Color c;
+	private int width, height;
 	
-	public Label(int x, int y, Font font, String text) {
+	public Label(GUIContext context, int x, int y, Font font, Color c, String text) {
+		super(context);
 		this.x = x;
 		this.y = y;
 		this.font = font;
+		this.c = c;
 		this.text = text;
+		this.width = font.getWidth(text);
+		this.height = font.getLineHeight();
 	}
 	
-	public Label(int x, int y, Font font) {
-		this(x,y,font,"");
-	}
-	
-	public void draw() {
-		font.drawString(x, y, text);
+	public Label(GUIContext context, int x, int y, Font font, Color c) {
+		this(context,x,y,font,c,"");
 	}
 
 	public void setText(String text) {
 		this.text = text;
+		this.width = font.getWidth(text);
+		this.height = font.getLineHeight();
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public void render(GUIContext context, Graphics g) throws SlickException {
+		font.drawString(x, y, text, c);
+		
+	}
+
+	@Override
+	public void setLocation(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 }
--- src/scene/ButtonScene.java
+++ src/scene/ButtonScene.java
@@ -1,28 +1,21 @@
 package scene;
 
-import org.newdawn.slick.Font;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.UnicodeFont;
-import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.Color;
+import org.newdawn.slick.*;
 import org.newdawn.slick.gui.*;
+import org.newdawn.slick.state.*;
 
-import component.Label;
+import component.*;
 
-
-import core.FontManager;
-import core.GameDirector;
+import core.*;
 
 public class ButtonScene extends Scene {
+	
 	public static SceneID ID = SceneID.ButtonScene;
-	TextField testField;
-	MouseOverArea testButton;
-	Label[] playerLabels;
-	Label titleLabel;
-	MouseOverArea[] playerSelection;
+	private TextField testField;
+	private MouseOverArea testButton;
+	private Label[] playerLabels;
+	private Label titleLabel;
+	private Spinner playerSpinner;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
@@ -30,13 +23,15 @@ public class ButtonScene extends Scene {
 		
 		Font fieldFont = GameDirector.sharedSceneDelegate().getFontManager().getFont(FontManager.FontID.FIELD);
 		Font h1 = GameDirector.sharedSceneDelegate().getFontManager().getFont(FontManager.FontID.H1);
-
+		playerSpinner = new Spinner(container, 400, 400, fieldFont, Color.white,
+				new Image("resources/up.png"), new Image("resources/down.png"), "Player 1",
+				"Player 2", "Player 3", "Player 4");
 		testField = new TextField(container, fieldFont, 40, 400, 300, 20);
 		testButton = new MouseOverArea(container, new Image("resources/button.png"), 600, 400, new ButtonListener() );
-		titleLabel = new Label( 50, 20, h1, "Button Scene");
+		titleLabel = new Label(container, 50, 20, h1, Color.white,"Button Scene");
 		playerLabels = new Label[4];
 		for (int i = 0; i < playerLabels.length; i++)
-			playerLabels[i] = new Label(60 + 160*i, 100, fieldFont, "Player " + (i+1) );
+			playerLabels[i] = new Label(container, 60 + 160*i, 100, fieldFont, Color.white, "Player " + (i+1) );
 		
 	}
 
@@ -48,9 +43,10 @@ public class ButtonScene extends Scene {
 		g.setColor(new Color(255,255,255));
 		testField.render(container, g);
 		testButton.render(container, g);
-		titleLabel.draw();
+		titleLabel.render(container, g);
+		playerSpinner.render(container, g);
 		for ( Label l : playerLabels )
-			l.draw();
+			l.render(container, g);
 		
 	}
 
@@ -63,12 +59,9 @@ public class ButtonScene extends Scene {
 	
 	private class ButtonListener implements ComponentListener {
 		public void componentActivated(AbstractComponent source) {
-			String str = testField.getText();
-			for (int i = 0; i < str.length(); i++) {
-				str = str.substring(1) + str.charAt(0);
-			}
-			playerLabels[0].setText(str);
+			playerLabels[playerSpinner.getState()].setText(testField.getText());
 		}
 	}
+
 
 }
