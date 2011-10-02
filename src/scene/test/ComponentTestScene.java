68a380e4130801ead7e4f51afdb059f704c64393
Tanner Smith <tanner@tssoftware.net>
2011-10-02 19:12:52 -0400

    Add a label that changes when we push the button.

--- src/scene/test/ComponentTestScene.java
+++ src/scene/test/ComponentTestScene.java
@@ -19,6 +19,7 @@ public class ComponentTestScene extends Scene {
 	public static SceneID ID = SceneID.ComponentTestScene;
 	private TextField textField;
 	private Button button;
+	private Label label;
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
@@ -35,12 +36,15 @@ public class ComponentTestScene extends Scene {
 		textField = new TextField(container, fieldFont, new Vector2f(40, 400), 300, 20);
 		mainLayer.add(textField);
 		
-		Label buttonLabel = new Label(container, new Vector2f(50, 20), h1, Color.white,"Button");
+		Label buttonLabel = new Label(container, new Vector2f(50, 20), h1, Color.white, "Button");
 		mainLayer.add(buttonLabel);
 		
 		button = new Button(container, new Label(container, fieldFont, Color.white, "my button"), new Vector2f(600, 400));
 		button.addListener(new ButtonListener());
 		mainLayer.add(button);
+		
+		label = new Label(container, h1, Color.white, "Label");
+		mainLayer.add(label);
 		
 //		Spinner playerSpinner = new Spinner(container, 400, 400, fieldFont, Color.white, new Image("resources/up.png"), new Image("resources/down.png"), false, "Player 1", "Player 2", "Player 3", "Player 4");
 //		mainLayer.add(playerSpinner);
@@ -67,6 +71,7 @@ public class ComponentTestScene extends Scene {
 	private class ButtonListener implements ComponentListener {
 		public void componentActivated(AbstractComponent source) {
 //			playerLabels[playerSpinner.getState()].setText(testField.getText() + playerSpinner2.getState());
+			label.setText("Button!");
 		}
 	}
 }
