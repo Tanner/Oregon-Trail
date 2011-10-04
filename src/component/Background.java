1ddd1d7bb8ec79d6d830b424dda5d7642761136a
jturner65 <jturner65@gatech.edu>
2011-10-04 19:50:43 -0400

    add javadoc stubs

--- src/component/Background.java
+++ src/component/Background.java
@@ -6,10 +6,22 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.gui.GUIContext;
 
+/**
+ * This class holds basic graphic utilities
+ * 
+ * @author 
+ *
+ */
+
 public class Background extends Component {
 	protected Vector2f location;
 	protected int width, height;
 	protected Color backgroundColor;
+	
+	/**
+	 * Constructor for Background object 
+	 * @param container gui item held within this object
+	 */
 	
 	public Background(GUIContext container) {
 		super(container);
@@ -18,30 +30,60 @@ public class Background extends Component {
 		this.height = container.getHeight();
 	}
 	
+	/**
+	 * Constructor for Background object
+	 * @param container gui item held within this object
+	 * @param backgroundColor color of background
+	 */
+	
 	public Background(GUIContext container, Color backgroundColor) {
 		this(container);
 		this.backgroundColor = backgroundColor;
 	}
+	
+	/**
+	 * @return the height of this object
+	 */
 
 	@Override
 	public int getHeight() {
 		return height;
 	}
 
+	/**
+	 * @return the width of this object
+	 */
+	
 	@Override
 	public int getWidth() {
 		return width;
 	}
-
+	
+	/**
+	 * @return the x position of this object
+	 */
+	
 	@Override
 	public int getX() {
 		return (int) location.x;
 	}
 
+	/**
+	 * @return the y position of this object
+	 */
+	
 	@Override
 	public int getY() {
 		return (int) location.y;
 	}
+	
+	/**
+	 * This method will draw the object
+	 * 
+	 * @param container
+	 * @param g
+	 * @throws SlickException
+	 */
 
 	@Override
 	public void render(GUIContext container, Graphics g) throws SlickException {
@@ -49,6 +91,12 @@ public class Background extends Component {
 		g.fillRect(location.x, location.y, width, height);
 	}
 
+	/**
+	 * Sets the location of the object on the screen
+	 * @param x
+	 * @param y
+	 */
+	
 	@Override
 	public void setLocation(int x, int y) {
 		if (location != null) {
@@ -56,10 +104,20 @@ public class Background extends Component {
 		}
 	}
 
+	/**
+	 * Sets the width of the object
+	 * @param width the new width
+	 */
+	
 	@Override
 	public void setWidth(int width) {
 		this.width = width;
 	}
+	
+	/**
+	 * Sets the height of the object
+	 * @param height the new height
+	 */
 
 	@Override
 	public void setHeight(int height) {
