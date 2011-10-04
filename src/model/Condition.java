b643a0bd95bda109ca1a46f44eaea82b62d27829
Tanner Smith <tanner@tssoftware.net>
2011-10-04 17:36:42 -0400

    Clean up Condition and Javadoc it.

--- src/model/Condition.java
+++ src/model/Condition.java
@@ -2,32 +2,64 @@ package model;
 
 import core.Logger;
 
+/**
+ * A condition with a min and max value and also a current value.
+ * 
+ * @author George Johnston
+ */
 public class Condition {
-
 	private int min, max, current;
 	
-	Condition(int min, int max, int current) {
+	/**
+	 * Constructor for Condition.
+	 * @param min Minimum value for the condition
+	 * @param max Maximum value for the condition
+	 * @param current Desired current value for the condition
+	 */
+	public Condition(int min, int max, int current) {
 		this.min = min;
 		this.max = max;
 		this.current = max;
 	}
 	
-	Condition(int min, int max) {
+	/**
+	 * Constructor for Condition. Sets the current value to the maximum value.
+	 * @param min Minimum value for the condition
+	 * @param max Maximum value for the condition
+	 */
+	public Condition(int min, int max) {
 		this(min, max, max);
 	}
 	
-	Condition(int max) {
+	/**
+	 * Constructor for Condition. Sets the minimum value to zero and the current value to the maximum value.
+	 * @param max Maximum value for the condition
+	 */
+	public Condition(int max) {
 		this(0, max, max);
 	}
 	
+	/**
+	 * Get the current value.
+	 * @return Current value
+	 */
 	public int getCurrent() {
 		return current;
 	}
 	
+	/**
+	 * Get the ratio of the current value to the maximum value.
+	 * @return Ratio of the current value to the maximum value
+	 */
 	public double getPercentage() {
-		return (double)current / max;
+		return ((double) current) / max;
 	}
 	
+	/**
+	 * Increase the current value by a desired amount.
+	 * @param amount Amount to increase the current value
+	 * @return Success status (true for success, false for fail)
+	 */
 	public boolean increase(int amount) {
 		if(amount <= 0) {
 			Logger.log("Not an increment", Logger.Level.ERROR);
@@ -44,6 +76,11 @@ public class Condition {
 		}
 	}
 	
+	/**
+	 * Decrease the current value by a desired amount.
+	 * @param amount Amount to decrease the current value
+	 * @return Success status (true for success, false for fail)
+	 */
 	public boolean decrease (int amount) {
 		if(amount <= 0) {
 			Logger.log("Not an decrement", Logger.Level.ERROR);
