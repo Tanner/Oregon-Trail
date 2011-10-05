eb69422cadd9def08c7b5f23f0a09090ea378e9c
jturner65 <jturner65@gatech.edu>
2011-10-04 22:24:24 -0400

    initial add of Constant Store class

---
+++ src/core/OTConstStore.java
@@ -0,0 +1,30 @@
+package core;
+
+import java.util.Collections;
+import java.util.HashMap; 
+import java.util.Map; 
+
+/**
+ * Class to hold constants used by OT game
+ * 
+ * @author john 
+ */
+
+public final class OTConstStore {
+	
+	private OTConstStore(){
+		
+	}
+	/**
+	 * initialize and build literal map used to hold all display literals in game
+	 */
+	public static final Map<String, String> LIT_MAP;
+	static{
+		Map<String,String> Literal_Map = new HashMap<String,String>(); 
+		Literal_Map.put("OT_TITLE","Oregon Trail");
+		//add literals here
+		LIT_MAP = Collections.unmodifiableMap(Literal_Map);
+	}
+	
+
+}
