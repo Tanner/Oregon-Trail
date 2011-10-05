e36e2d5535dd1d5b0f47f87631bc3076c338830c
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-10-05 10:48:48 -0400

    Increase font size for h1 and h2.

--- src/core/FontManager.java
+++ src/core/FontManager.java
@@ -25,13 +25,13 @@ public class FontManager {
 	public void init() {
 		try {
 			String glyphsToAdd = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890- .,!\"'?$";
-			h1 = new UnicodeFont("resources/04B_03__.ttf", 36, true, false);
+			h1 = new UnicodeFont("resources/04B_03__.ttf", 42, true, false);
 //			h1.addAsciiGlyphs();
 			h1.addGlyphs(glyphsToAdd);
 			h1.getEffects().add(new ColorEffect());
 			h1.loadGlyphs();
 			
-			h2 = new UnicodeFont("resources/04B_03__.ttf", 24, true, false);
+			h2 = new UnicodeFont("resources/04B_03__.ttf", 28, true, false);
 			h2.addGlyphs(glyphsToAdd);
 			h2.getEffects().add(new ColorEffect());
 			h2.loadGlyphs();
