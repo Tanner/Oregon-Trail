f707667e12b404812ca07c91b796fe23f941fd07
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-10-05 11:04:08 -0400

    Add 1px drop shadow to fonts.

--- src/core/FontManager.java
+++ src/core/FontManager.java
@@ -1,8 +1,12 @@
 package core;
 
+import java.awt.Color;
+
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.font.effects.ColorEffect;
+import org.newdawn.slick.font.effects.OutlineEffect;
+import org.newdawn.slick.font.effects.ShadowEffect;
 
 /**
  * Manages fonts for use in the game.
@@ -28,17 +32,23 @@ public class FontManager {
 			h1 = new UnicodeFont("resources/04B_03__.ttf", 42, true, false);
 //			h1.addAsciiGlyphs();
 			h1.addGlyphs(glyphsToAdd);
+			h1.getEffects().add(new ShadowEffect(Color.black, 0, 1, 1f));
 			h1.getEffects().add(new ColorEffect());
+			h1.setPaddingBottom(1);
 			h1.loadGlyphs();
 			
 			h2 = new UnicodeFont("resources/04B_03__.ttf", 28, true, false);
 			h2.addGlyphs(glyphsToAdd);
+			h2.getEffects().add(new ShadowEffect(Color.black, 0, 1, 1f));
 			h2.getEffects().add(new ColorEffect());
+			h2.setPaddingBottom(1);
 			h2.loadGlyphs();
 			
 			fieldFont = new UnicodeFont("resources/04B_03__.ttf", 20, true, false);
 			fieldFont.addGlyphs(glyphsToAdd);
+			fieldFont.getEffects().add(new ShadowEffect(Color.black, 0, 1, 1f));
 			fieldFont.getEffects().add(new ColorEffect());
+			fieldFont.setPaddingBottom(1);
 			fieldFont.loadGlyphs();
 			
 		} catch (SlickException e) {
