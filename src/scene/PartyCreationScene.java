30b34496e1eb496e85501de3b82df8a74d3c92ca
Tanner Smith <tanner@tssoftware.net>
2011-10-04 22:43:00 -0400

    Create some constants for PCS.

--- src/scene/PartyCreationScene.java
+++ src/scene/PartyCreationScene.java
@@ -10,7 +10,6 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import component.Background;
 import component.Button;
-import component.Component;
 import component.Label;
 import component.Label.Alignment;
 import component.Modal;
@@ -23,38 +22,40 @@ import core.Logger;
 
 public class PartyCreationScene extends Scene {
 	public static final SceneID ID = SceneID.PartyCreation;
-	private static final int PADDING = 20;
 	
-	private Button newPersonButtons[] = new Button[4];
-	private TextField personNameTextFields[] = new TextField[4];
-	private Button personProfessionButtons[] = new Button[4];
-	private Label personMoneyLabels[] = new Label[4];
-	private Button personSkillOneButtons[] = new Button[4];
-	private Button personSkillTwoButtons[] = new Button[4];
-	private Button personSkillThreeButtons[] = new Button[4];
+	private static final int PADDING = 20;
+	private static final int NUM_PEOPLE = 4;
+	private static final int newPersonButtonHeight = 150;
+	private static final int regularButtonHeight = 30;
+	
+	private Button newPersonButtons[] = new Button[NUM_PEOPLE];
+	private TextField personNameTextFields[] = new TextField[NUM_PEOPLE];
+	private Button personProfessionButtons[] = new Button[NUM_PEOPLE];
+	private Label personMoneyLabels[] = new Label[NUM_PEOPLE];
+	private Button personSkillOneButtons[] = new Button[NUM_PEOPLE];
+	private Button personSkillTwoButtons[] = new Button[NUM_PEOPLE];
+	private Button personSkillThreeButtons[] = new Button[NUM_PEOPLE];
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		super.init(container, game);
 		
 		UnicodeFont fieldFont = GameDirector.sharedSceneDelegate().getFontManager().getFont(FontManager.FontID.FIELD);
-		int buttonCount = 4;
-		int buttonWidth = (container.getWidth() - PADDING * (buttonCount + 1)) / buttonCount;
-		int buttonheight = 150;
+		int buttonWidth = (container.getWidth() - PADDING * (NUM_PEOPLE + 1)) / NUM_PEOPLE;
 
 		for (int i = 0; i < newPersonButtons.length; i++) {
 			Positionable newPersonButton = (i == 0) ? mainLayer : newPersonButtons[i - 1];
 			ReferencePoint newPersonButtonReferencePoint = (i == 0) ? ReferencePoint.TopLeft :ReferencePoint.TopRight;
 			int newPersonButtonPaddingY = (i == 0) ? PADDING : 0;
 			
-			newPersonButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "New Player"), buttonWidth, 150);
+			newPersonButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "New Player"), buttonWidth, newPersonButtonHeight);
 			newPersonButtons[i].addListener(new PersonListener());
 			mainLayer.add(newPersonButtons[i], newPersonButton.getPosition(newPersonButtonReferencePoint), Positionable.ReferencePoint.TopLeft, PADDING, newPersonButtonPaddingY);
 			
-			personNameTextFields[i] = new TextField(container, fieldFont, buttonWidth, 40);
+			personNameTextFields[i] = new TextField(container, fieldFont, buttonWidth, regularButtonHeight);
 			mainLayer.add(personNameTextFields[i], newPersonButtons[i].getPosition(Positionable.ReferencePoint.BottomLeft), Positionable.ReferencePoint.TopLeft, 0, PADDING);
 			
-			personProfessionButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Profession"), buttonWidth, 40);
+			personProfessionButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Profession"), buttonWidth, regularButtonHeight);
 			personProfessionButtons[i].addListener(new PersonListener());
 			mainLayer.add(personProfessionButtons[i], personNameTextFields[i].getPosition(ReferencePoint.BottomLeft), ReferencePoint.TopLeft, 0, PADDING);
 			
@@ -62,15 +63,15 @@ public class PartyCreationScene extends Scene {
 			personMoneyLabels[i].setAlignment(Alignment.Center);
 			mainLayer.add(personMoneyLabels[i], personProfessionButtons[i].getPosition(ReferencePoint.BottomLeft), ReferencePoint.TopLeft, 0, PADDING);
 			
-			personSkillOneButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Skill 1"), buttonWidth, 40);
+			personSkillOneButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Skill 1"), buttonWidth, regularButtonHeight);
 			personSkillOneButtons[i].addListener(new PersonListener());
 			mainLayer.add(personSkillOneButtons[i], personMoneyLabels[i].getPosition(ReferencePoint.BottomLeft), ReferencePoint.TopLeft, 0, PADDING);
 			
-			personSkillTwoButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Skill 2"), buttonWidth, 40);
+			personSkillTwoButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Skill 2"), buttonWidth, regularButtonHeight);
 			personSkillTwoButtons[i].addListener(new PersonListener());
 			mainLayer.add(personSkillTwoButtons[i], personSkillOneButtons[i].getPosition(ReferencePoint.BottomLeft), ReferencePoint.TopLeft, 0, PADDING);
 			
-			personSkillThreeButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Skill 3"), buttonWidth, 40);
+			personSkillThreeButtons[i] = new Button(container, new Label(container, fieldFont, Color.white, "Skill 3"), buttonWidth, regularButtonHeight);
 			personSkillThreeButtons[i].addListener(new PersonListener());
 			mainLayer.add(personSkillThreeButtons[i], personSkillTwoButtons[i].getPosition(ReferencePoint.BottomLeft), ReferencePoint.TopLeft, 0, PADDING);
 		}
