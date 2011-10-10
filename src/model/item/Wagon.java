095c133c4483200d979bf496065fba671291d3da
George Johnston <hankrearden1221@gmail.com>
2011-10-10 15:42:17 -0400

    Basics of buy in party.  Constructor change for item, vehicle.

--- src/model/item/Wagon.java
+++ src/model/item/Wagon.java
@@ -4,6 +4,6 @@ import model.Condition;
 
 public class Wagon extends Vehicle {
 	public Wagon() {
-		super("Wagon", "This is a wagon", new Condition(100), 2000, 200);
+		super("Wagon", "This is a wagon", new Condition(100), 2000, 200, 2000);
 	}
 }
