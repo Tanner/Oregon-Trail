f3a2a9b8695fbda518901573e282ff74f3a5ca27
Tanner Smith <tanner@tssoftware.net>
2011-10-11 19:40:36 -0400

    Use the SlotConditionGroup in the OIB. Remove old way of doing things.

--- src/component/OwnerInventoryButtons.java
+++ src/component/OwnerInventoryButtons.java
@@ -12,6 +12,8 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
 
 import component.Positionable.ReferencePoint;
 
@@ -24,6 +26,7 @@ public class OwnerInventoryButtons {
 	private static final int PADDING = 20;
 	
 	private Person person;
+	private ArrayList<SlotConditionGroup> itemSlots;
 	private Font font;
 	
 	public OwnerInventoryButtons(Person person) {
@@ -46,13 +49,13 @@ public class OwnerInventoryButtons {
 			
 			if (i == 0) {
 				position = lastPositionReference.getPosition(ReferencePoint.TopLeft);
-			}
-			
-			if (i == 0 || i == items.size()) {
+				padding = 0;
+			} else if (i == items.size()) {
 				padding = 0;
 			}
 			
-			lastPositionReference = createItemButton(container, items, position, font, itemPanel, padding);
+			itemSlots.set(i, new SlotConditionGroup(container, ITEM_BUTTON_WIDTH, panelHeight, font, i, items));
+			itemPanel.add(itemSlots.get(i), position, ReferencePoint.TopLeft, padding, 0);
 		}
 		
 		Label nameLabel = new Label(container, font, Color.white, person.getName());
@@ -60,20 +63,6 @@ public class OwnerInventoryButtons {
 		Panel panel = new Panel(container, panelWidth, panelHeight + NAME_PADDING + (int)nameLabel.getFontHeight());
 		panel.add(nameLabel, panel.getPosition(ReferencePoint.TopLeft), ReferencePoint.TopLeft, 0, 0);
 		panel.add(itemPanel, nameLabel.getPosition(ReferencePoint.BottomLeft), ReferencePoint.TopLeft, 0, NAME_PADDING);
-	}
-	
-	public Button createItemButton(GameContainer container, PriorityQueue<Item> items, Vector2f position, Font font, Panel panel, int offset) {	
-		CountingButton button = new CountingButton(container, ITEM_BUTTON_WIDTH, ITEM_BUTTON_HEIGHT, new Label(container, font, Color.white, items.peek().getName()));
-		button.setCountUpOnLeftClick(false);
-		button.setMax(items.size());
-		button.setCount(items.size());
-		
-		ConditionBar conditionBar = new ConditionBar(container, ITEM_BUTTON_WIDTH, ITEM_CONDITION_BAR_HEIGHT, items.peek().getStatus());
-		
-		panel.add(button, position, ReferencePoint.TopLeft, offset, 0);
-		panel.add(conditionBar, button.getPosition(ReferencePoint.BottomCenter), ReferencePoint.TopCenter, 0, CONDITION_BAR_PADDING);
-		
-		return button;
 	}
 
 	/**
