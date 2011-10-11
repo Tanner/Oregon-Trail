132818fafe68d76e8bdcc9ce4c6b5e838502405a
George Johnston <hankrearden1221@gmail.com>
2011-10-11 18:41:20 -0400

    person to inventoried in party.buyItemForInventory

--- src/model/Party.java
+++ src/model/Party.java
@@ -53,13 +53,13 @@ public class Party {
 		Logger.log("Current pace is: " + currentPace + " and current rations is: " + currentRations, Level.INFO);
 	}
 	
-	public boolean buyItemForInventory(ArrayList<Item> items, Person person) {
+	public boolean buyItemForInventory(ArrayList<Item> items, Inventoried buyer) {
 		int cost = 0;
 		for(Item item : items) {
 			cost += item.getCost();
 		}
-		if (money > cost && person.canGetItem(items)) {
-			person.addItemToInventory(items);
+		if (money > cost && buyer.canGetItem(items)) {
+			buyer.addItemToInventory(items);
 			money -= cost;
 			return true;
 		} else {
