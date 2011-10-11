36805a0f83bde8759dee064c08b04bebb9c98264
Tanner Smith <tanner@tssoftware.net>
2011-10-11 14:07:59 -0400

    Add function button to PIS that changes functionality from sell to drop depending on whether you're coming from StoreScene or elsewhere.

--- src/core/GameDirector.java
+++ src/core/GameDirector.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
 import scene.*;
+import scene.PartyInventoryScene.EXTRA_BUTTON_FUNC;
 import scene.test.*;
 
 import model.*;
@@ -77,7 +78,7 @@ public class GameDirector implements SceneDelegate, SceneDirectorDelegate {
 		case Store:
 			return new StoreScene(game.getPlayer().getParty());
 		case PartyInventory:
-			return new PartyInventoryScene(game.getPlayer().getParty());
+			return new PartyInventoryScene(game.getPlayer().getParty(), EXTRA_BUTTON_FUNC.DROP);
 		case SceneSelector:
 			return new SceneSelectorScene(game.getPlayer());
 		case ComponentTest:
@@ -118,14 +119,14 @@ public class GameDirector implements SceneDelegate, SceneDirectorDelegate {
 			// Last scene was Store Scene
 			if (id == SceneID.PartyInventory) {
 				// Requested Party Inventory Scene
-				newScene = new PartyInventoryScene(game.getPlayer().getParty());
+				newScene = new PartyInventoryScene(game.getPlayer().getParty(), EXTRA_BUTTON_FUNC.SELL);
 			}
 		} else if (lastScene instanceof SceneSelectorScene) {
 			// Last scene was Scene Selector Scene
 			newScene =  sceneForSceneID(id);
 		} else if (id == SceneID.PartyInventory) {
 			// Requested Party Inventory Scene
-			newScene = new PartyInventoryScene(game.getPlayer().getParty());
+			newScene = new PartyInventoryScene(game.getPlayer().getParty(), EXTRA_BUTTON_FUNC.DROP);
 		}
 		
 		if (newScene != null) {
