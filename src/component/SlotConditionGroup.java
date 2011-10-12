fad5e0f1e3a5af601570b954a5f055df8dc69131
Tanner Smith <tanner@tssoftware.net>
2011-10-11 20:14:10 -0400

    Make slot condition group handle null items.

--- src/component/SlotConditionGroup.java
+++ src/component/SlotConditionGroup.java
@@ -12,34 +12,31 @@ import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.ComponentListener;
 import org.newdawn.slick.gui.GUIContext;
 
-import component.Positionable.ReferencePoint;
-
 public class SlotConditionGroup extends Component {
 	private static final int ITEM_BUTTON_HEIGHT = 40;
 	private static final int ITEM_CONDITION_BAR_HEIGHT = 5;
 	
-	private Panel panel;
 	private CountingButton button;
 	private ConditionBar conditionBar;
 	
 	private int slotNumber;
 	
-	public SlotConditionGroup(GUIContext context, int width, int height, Font font, int slotNumber, PriorityQueue<Item> items) {
-		super(context, width, height);
+	public SlotConditionGroup(GUIContext container, int width, int height, Font font, int slotNumber, PriorityQueue<Item> items) {
+		super(container, width, height);
 		
 		this.slotNumber = slotNumber;
 		
-		CountingButton button = new CountingButton(container, width, ITEM_BUTTON_HEIGHT, new Label(container, font, Color.white, items.peek().getName()));
-		button.setCountUpOnLeftClick(false);
-		button.setMax(items.size());
-		button.setCount(items.size());
-		button.addListener(new ButtonListener());
-		add(button, this.getPosition(ReferencePoint.TopLeft), ReferencePoint.TopLeft, 0, 0);
+		button = new CountingButton(container, width, ITEM_BUTTON_HEIGHT, new Label(container, font, Color.white, ""));
+		conditionBar = new ConditionBar(container, width, ITEM_CONDITION_BAR_HEIGHT, null);
 		
-		ConditionBar conditionBar = new ConditionBar(container, width, ITEM_CONDITION_BAR_HEIGHT, items.peek().getStatus());		
+		changeContents(items);
+		
+		button.setCountUpOnLeftClick(false);
+		button.addListener(new ButtonListener());
 		
 		int padding = height - (ITEM_BUTTON_HEIGHT + ITEM_CONDITION_BAR_HEIGHT);
 		
+		add(button, this.getPosition(ReferencePoint.TopLeft), ReferencePoint.TopLeft, 0, 0);
 		add(conditionBar, button.getPosition(ReferencePoint.BottomCenter), ReferencePoint.TopCenter, 0, padding);
 	}
 
@@ -57,10 +54,21 @@ public class SlotConditionGroup extends Component {
 	}
 	
 	public void changeContents(PriorityQueue<Item> items) {
-		button.setText(items.peek().getName());
-		button.setCount(items.size());
-		
-		conditionBar.setCondition(items.peek().getStatus());
+		if (items != null && items.size() > 1) {
+			Item topItem = items.peek();
+			
+			button.setText(topItem.getName());
+			button.setDisabled(false);
+			button.setCount(items.size());
+			button.setMax(items.size());
+			
+			conditionBar.setCondition(topItem.getStatus());
+		} else {
+			button.setText("None");
+			button.setDisabled(true);
+			
+			conditionBar.setCondition(null);
+		}
 	}
 	
 	private class ButtonListener implements ComponentListener {
