7f593467d61db7f500d301367e979a40064d4328
Tanner Smith <tanner@tssoftware.net>
2011-10-20 19:11:43 -0400

    Add health lists for Vehicle. Use addAsRow.

--- src/component/HUD.java
+++ src/component/HUD.java
@@ -17,7 +17,7 @@ public class HUD extends Component {
 	private static final int CONDITION_BAR_HEIGHT = 10;
 	private static final int CONDITION_BAR_PADDING = 4;
 	
-	private ComponentConditionGroup<Label> memberHealthGroup;
+	private ComponentConditionGroup<Label> memberHealthGroup, vehicleStatusGroup;
 	
 	public HUD(GUIContext context, HUDDataSource data) {
 		super(context, context.getWidth(), context.getHeight());
@@ -26,9 +26,18 @@ public class HUD extends Component {
 		
 		int height = fieldFont.getLineHeight() + CONDITION_BAR_HEIGHT + CONDITION_BAR_PADDING;
 
-		Label label = new Label(context, BUTTON_WIDTH, fieldFont, Color.white, "Party Members");
-		memberHealthGroup = new ComponentConditionGroup<Label>(context, BUTTON_WIDTH, height, CONDITION_BAR_HEIGHT, label, data.getPartyMembersHealth());
+		ComponentConditionGroup<Label> groups[] = (ComponentConditionGroup<Label>[])new ComponentConditionGroup[2];
 		
-		add(memberHealthGroup, getPosition(ReferencePoint.TOPLEFT), ReferencePoint.TOPLEFT, PADDING, PADDING);
+		// Party Members Health
+		Label partyMembersHealthLabel = new Label(context, BUTTON_WIDTH, fieldFont, Color.white, "Party Members");
+		memberHealthGroup = new ComponentConditionGroup<Label>(context, BUTTON_WIDTH, height, CONDITION_BAR_HEIGHT, partyMembersHealthLabel, data.getPartyMembersHealth());
+		groups[0] = memberHealthGroup;
+		
+		// Vehicle Status
+		Label vehicleStatusLabel = new Label(context, BUTTON_WIDTH, fieldFont, Color.white, "Vehicle");
+		vehicleStatusGroup = new ComponentConditionGroup<Label>(context, BUTTON_WIDTH, height, CONDITION_BAR_HEIGHT, vehicleStatusLabel, data.getVehicleStatus());
+		groups[1] = vehicleStatusGroup;
+		
+		addAsRow(groups, getPosition(ReferencePoint.TOPLEFT), PADDING, PADDING, PADDING);
 	}
 }
