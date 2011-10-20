c8c5eb10e35e636c2f9bdf7e47ed4dc3a963ddad
Tanner Smith <tanner@tssoftware.net>
2011-10-20 19:22:57 -0400

    Rename BUTTON_WIDTH to WIDTH. Add labels to ConstantStore.

--- src/component/HUD.java
+++ src/component/HUD.java
@@ -6,13 +6,14 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.gui.GUIContext;
 
+import core.ConstantStore;
 import core.FontManager;
 import core.GameDirector;
 
 public class HUD extends Component {
 	private static final int PADDING = 20;
 	
-	private static final int BUTTON_WIDTH = 200;
+	private static final int WIDTH = 150;
 	
 	private static final int CONDITION_BAR_HEIGHT = 10;
 	private static final int CONDITION_BAR_PADDING = 4;
@@ -29,13 +30,13 @@ public class HUD extends Component {
 		ComponentConditionGroup<Label> groups[] = (ComponentConditionGroup<Label>[])new ComponentConditionGroup[2];
 		
 		// Party Members Health
-		Label partyMembersHealthLabel = new Label(context, BUTTON_WIDTH, fieldFont, Color.white, "Party Members");
-		memberHealthGroup = new ComponentConditionGroup<Label>(context, BUTTON_WIDTH, height, CONDITION_BAR_HEIGHT, partyMembersHealthLabel, data.getPartyMembersHealth());
+		Label partyMembersHealthLabel = new Label(context, WIDTH, fieldFont, Color.white, ConstantStore.get("HUD_SCENE", "MEMBERS_HEALTH"));
+		memberHealthGroup = new ComponentConditionGroup<Label>(context, WIDTH, height, CONDITION_BAR_HEIGHT, partyMembersHealthLabel, data.getPartyMembersHealth());
 		groups[0] = memberHealthGroup;
 		
 		// Vehicle Status
-		Label vehicleStatusLabel = new Label(context, BUTTON_WIDTH, fieldFont, Color.white, "Vehicle");
-		vehicleStatusGroup = new ComponentConditionGroup<Label>(context, BUTTON_WIDTH, height, CONDITION_BAR_HEIGHT, vehicleStatusLabel, data.getVehicleStatus());
+		Label vehicleStatusLabel = new Label(context, WIDTH, fieldFont, Color.white, ConstantStore.get("HUD_SCENE", "VEHICLE_HEALTH"));
+		vehicleStatusGroup = new ComponentConditionGroup<Label>(context, WIDTH, height, CONDITION_BAR_HEIGHT, vehicleStatusLabel, data.getVehicleStatus());
 		groups[1] = vehicleStatusGroup;
 		
 		addAsRow(groups, getPosition(ReferencePoint.TOPLEFT), PADDING, PADDING, PADDING);
