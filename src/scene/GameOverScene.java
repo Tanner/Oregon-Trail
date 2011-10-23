728f00c322c364218fb881412e1fd2d9d2d1d6ab
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-10-23 17:14:46 -0400

    Customize appearance of GameOverScene.

--- src/scene/GameOverScene.java
+++ src/scene/GameOverScene.java
@@ -1,12 +1,16 @@
 package scene;
 
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import component.Label;
 import component.Panel;
+import component.Positionable;
+import core.FontManager;
+import core.GameDirector;
 
 public class GameOverScene extends Scene {
 	public static final SceneID ID = SceneID.HUNT;
@@ -15,7 +19,11 @@ public class GameOverScene extends Scene {
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		super.init(container, game);
 		
-		backgroundLayer.add(new Panel(container, Color.blue));
+		Font h1 = GameDirector.sharedSceneListener().getFontManager().getFont(FontManager.FontID.H1);		
+		Label titleLabel = new Label(container, h1, Color.red, "Game Over");
+		mainLayer.add(titleLabel, mainLayer.getPosition(Positionable.ReferencePoint.CENTERCENTER), Positionable.ReferencePoint.CENTERCENTER);
+		
+		backgroundLayer.add(new Panel(container, Color.black));
 	}
 	
 	@Override
