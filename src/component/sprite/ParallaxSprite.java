bfd662e139bfbb8dc674f0aba730ae7cf3a88823
Tanner Smith <tanner@tssoftware.net>
2011-10-24 16:35:28 -0400

    Fix ParallaxSprite so that your images that you what to "parallax" aren't as big as the screen.

--- src/component/sprite/ParallaxSprite.java
+++ src/component/sprite/ParallaxSprite.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.gui.GUIContext;
 import org.newdawn.slick.Image;
 
 import component.Component;
+import component.Panel;
 
 /**
  * A sprite that shifts a certain amount on the screen and loops around.
@@ -11,6 +12,9 @@ import component.Component;
 public class ParallaxSprite extends Component {
 	private Sprite spriteA;
 	private Sprite spriteB;
+	
+	private Panel panelA;
+	private Panel panelB;
 	
 	private int elapsedTime;
 	private final int maxElapsedTime;
@@ -30,8 +34,14 @@ public class ParallaxSprite extends Component {
 		spriteA = new Sprite(context, spriteWidth, image);
 		spriteB = new Sprite(context, spriteWidth, image);
 		
-		add(spriteA, getPosition(ReferencePoint.BOTTOMLEFT), ReferencePoint.BOTTOMLEFT);
-		add(spriteB, spriteA.getPosition(ReferencePoint.BOTTOMRIGHT), ReferencePoint.BOTTOMLEFT);
+		panelA = new Panel(context, spriteA.getWidth(), spriteA.getHeight());
+		panelA.add(spriteA, getPosition(ReferencePoint.TOPLEFT), ReferencePoint.TOPLEFT);
+		
+		panelB = new Panel(context, spriteB.getWidth(), spriteB.getHeight());
+		panelB.add(spriteB, getPosition(ReferencePoint.TOPLEFT), ReferencePoint.TOPLEFT);
+		
+		add(panelA, getPosition(ReferencePoint.BOTTOMLEFT), ReferencePoint.BOTTOMLEFT);
+		add(panelB, panelA.getPosition(ReferencePoint.BOTTOMLEFT), ReferencePoint.BOTTOMLEFT, context.getWidth(), 0);
 		
 		this.maxElapsedTime = maxElapsedTime;
 	}
@@ -44,17 +54,17 @@ public class ParallaxSprite extends Component {
 		elapsedTime += delta;
 		
 		if (elapsedTime > maxElapsedTime) {
-			spriteA.setLocation(spriteA.getX() + DELTA_X, spriteA.getY());
-			spriteB.setLocation(spriteB.getX() + DELTA_X, spriteB.getY());
+			panelA.setLocation(panelA.getX() + DELTA_X, panelA.getY());
+			panelB.setLocation(panelB.getX() + DELTA_X, panelB.getY());
 			elapsedTime = 0;
 		}
 		
-		if (spriteA.getX() > container.getWidth()) {
-			spriteA.setLocation(spriteB.getX() - container.getWidth(), spriteA.getY());
+		if (panelA.getX() > container.getWidth()) {
+			panelA.setLocation(panelB.getX() - container.getWidth(), panelA.getY());
 		}
 		
-		if (spriteB.getX() > container.getWidth()) {
-			spriteB.setLocation(spriteA.getX() - container.getWidth(), spriteB.getY());
+		if (panelB.getX() > container.getWidth()) {
+			panelB.setLocation(panelA.getX() - container.getWidth(), panelB.getY());
 		}
 	}
 }
