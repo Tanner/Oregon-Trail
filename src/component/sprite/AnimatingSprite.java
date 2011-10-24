b9094e8eb4b8592e527e13ce5a8e88c2bb03750c
Ryan Ashcraft <ryanashcraft@gmail.com>
2011-10-23 23:22:45 -0400

    Extract features from Sprite and move them to new subclass called AnimatingSprite.

---
+++ src/component/sprite/AnimatingSprite.java
@@ -0,0 +1,68 @@
+package component.sprite;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.gui.GUIContext;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class AnimatingSprite extends Sprite {
+	private Animation leftAnimation;
+	private Animation rightAnimation;
+	private Animation currentAnimation;
+	
+	public static enum DirectionFacing { LEFT, RIGHT }
+	private DirectionFacing xDirection = DirectionFacing.LEFT;
+	
+	/**
+	 * Constructs a {@code Sprite} with a right and left animation.
+	 * @param context The GUI context
+	 * @param rightAnimation Animation for when facing right
+	 * @param leftAnimation Animation for when facing left
+	 */
+	public AnimatingSprite(GUIContext context, Animation rightAnimation, Animation leftAnimation) {
+		super(context, rightAnimation.getWidth(), rightAnimation.getHeight());
+				
+		this.rightAnimation = rightAnimation;
+		this.leftAnimation = leftAnimation;
+		
+		xDirection = DirectionFacing.LEFT;
+		currentAnimation = leftAnimation;
+
+		setImage(currentAnimation.getCurrentFrame());
+	}
+	
+	/**
+	 * Updates the Sprite on a clock cycle.
+	 * @param container Container displaying the component
+	 * @param game Game containing the Entity
+	 * @param delta Time since last update
+	 */
+	public void update(GameContainer container, int delta) {
+		if (xDirection == DirectionFacing.LEFT) {
+			currentAnimation = leftAnimation;
+		} else {
+			currentAnimation = rightAnimation;
+		}
+		
+		setImage(currentAnimation.getCurrentFrame());
+
+		currentAnimation.update(delta);
+	}
+	
+	/**
+	 * Get which direction the Sprite is facing.
+	 * @return Direction the Sprite is facing
+	 */
+	public DirectionFacing getDirectionFacing() {
+		return xDirection;
+	}
+	
+	/**
+	 * Sets the direction the Sprite is facing
+	 * @param xDirection New direction the Sprite is facing
+	 */
+	public void setDirectionFacing(DirectionFacing xDirection) {
+		this.xDirection = xDirection;
+	}
+}
--- src/component/sprite/Sprite.java
+++ src/component/sprite/Sprite.java
@@ -1,12 +1,9 @@
 package component.sprite;
 
-import org.newdawn.slick.Animation;
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.gui.GUIContext;
-import org.newdawn.slick.state.StateBasedGame;
 
 import component.Component;
 
@@ -15,124 +12,36 @@ import component.Component;
  * to represent a sprite with an left and right animation.
  */
 public class Sprite extends Component {
-	private Animation leftAnimation;
-	private Animation rightAnimation;
-	private Animation currentAnimation;
-
-	private float rotation;
-	private float scale;
+	private Image image;
 	
-	public static enum DirectionFacing { LEFT, RIGHT }
-	private DirectionFacing xDirection = DirectionFacing.LEFT;
-	
-	/**
-	 * Constructs a {@code Sprite} with a right and left animation.
-	 * @param context The GUI context
-	 * @param rightAnimation Animation for when facing right
-	 * @param leftAnimation Animation for when facing left
-	 */
-	public Sprite(GUIContext context, Animation rightAnimation, Animation leftAnimation) {
-		super(context, rightAnimation.getWidth(), rightAnimation.getHeight());
-				
-		this.rightAnimation = rightAnimation;
-		this.leftAnimation = leftAnimation;
-		
-		xDirection = DirectionFacing.LEFT;
-		currentAnimation = leftAnimation;
-		
-		scale = 1;
-		rotation = 0;
-	}
-	
-	/**
-	 * Constructs a {@code Sprite} with a right and left image..
-	 * @param container The GUI context
-	 * @param rightImage Image for when facing right
-	 * @param leftImage Image for when facing left
-	 */
-	public Sprite(GUIContext context, Image rightImage, Image leftImage) {
-		this(context, new Animation(new Image[]{rightImage}, 1), new Animation(new Image[]{leftImage}, 1));
-	}
-	
-	public Sprite(GUIContext context, Image image) {
-		this(context, image, image);
+	public Sprite(GUIContext context, int width, int height) {
+		super(context, width, height);
 	}
 	
 	public Sprite(GUIContext context, int width, Image image) {
-		super(context, width, width * image.getHeight() / image.getWidth());
+		this(context, width, width * image.getHeight() / image.getWidth());
 		
-		this.leftAnimation = new Animation(new Image[]{image}, 1);
-		currentAnimation = leftAnimation;
+		this.image = image;
 	}
 	
+	public Sprite(GUIContext context, Image image) {
+		this(context, image.getWidth(), image);
+	}
+
 	@Override
 	public void render(GUIContext context, Graphics g) throws SlickException {
-		super.render(context, g);
-
-		currentAnimation.draw(getX(), getY(), getWidth(), getHeight());
-	}
-	
-	/**
-	 * Updates the Sprite on a clock cycle.
-	 * @param container Container displaying the component
-	 * @param game Game containing the Entity
-	 * @param delta Time since last update
-	 */
-	public void update(GameContainer container, StateBasedGame game, int delta) {
-		if (xDirection == DirectionFacing.LEFT) {
-			currentAnimation = leftAnimation;
-		} else {
-			currentAnimation = rightAnimation;
+		if (!isVisible()) {
+			return;
 		}
 		
-		currentAnimation.update(delta);
+		super.render(context, g);
+
+		if (image != null) {
+			image.draw(getX(), getY(), getWidth(), getHeight());
+		}
 	}
 	
-	/**
-	 * Get the rotation.
-	 * @return Rotation of the Sprite
-	 */
-	public float getRotation() {
-		return rotation;
-	}
-	
-	/**
-	 * Get which direction the Sprite is facing.
-	 * @return Direction the Sprite is facing
-	 */
-	public DirectionFacing getDirectionFacing() {
-		return xDirection;
-	}
-	
-	/**
-	 * Get the scale.
-	 * @return Scale of the Sprite
-	 */
-	public float getScale() {
-		return scale;
-	}
-	
-	/**
-	 * Set the scale of the Sprite
-	 * @param scale New scale
-	 */
-	public void setScale(float scale) {
-		this.scale = scale;
-	}
-	
-	/**
-	 * Set the rotation of the Sprite
-	 * @param rotation New rotation in degrees
-	 */
-	public void setRotation(float rotation) {
-		this.rotation = rotation;
-	}
-	
-	/**
-	 * Sets the direction the Sprite is facing
-	 * @param xDirection New direction the Sprite is facing
-	 */
-	public void setDirectionFacing(DirectionFacing xDirection) {
-		this.xDirection = xDirection;
+	public void setImage(Image image) {
+		this.image = image;
 	}
 }
