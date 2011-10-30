7e5c42900fbaec6c03ff655bed539d84e9b83db1
George Johnston <hankrearden1221@gmail.com>
2011-10-30 01:48:10 -0400

    Slight aesthetic changes to name layout in town scene trail chooser.

--- src/scene/TownScene.java
+++ src/scene/TownScene.java
@@ -55,10 +55,10 @@ public class TownScene extends Scene {
 			String[] trails = new String[location.getTrails()];
 			for(int i = 0; i < location.getTrails(); i++) {
 				TrailEdge temp = location.getOutBoundTrailByIndex(i);
-				trails[i] = temp.getDangerRating() + ", " + temp.getRoughLength() + ", " + temp.getRoughDirection() + temp.getName();
+				trails[i] = temp.getRoughDirection() + " " + temp.getDangerRating() + ", " + temp.getRoughLength() + " \n" + temp.getName();
 				}
 			trailChoiceModal = new ComponentModal<SegmentedControl>(container, this, ConstantStore.get("TOWN_SCENE", "TRAIL_CHOICE"),
-					new SegmentedControl(container, 850, 200, 3, 1, 20, true, 1, trails));
+					new SegmentedControl(container, 700, 300, 3, 1, 20, true, 1, trails));
 		}
 	}
 	
